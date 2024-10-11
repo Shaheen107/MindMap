21d2a315a9f068d62d3bd8130517dbfcf380fafc
dsv <zxcv>
2024-10-11 22:31:33 +0500

    Initial Commit

---
+++ MindMap/MindMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindMapApp.swift
+//  MindMap
+//
+//  Created by Muhammad Rizwan on 11/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct MindMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
